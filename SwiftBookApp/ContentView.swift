0948f958b2888e2ed026ec0def88f2ac93e25afc
takezo <taketo45@gmail.com>
2025-02-07 19:18:10 +0900

    Initial Commit

---
+++ SwiftBookApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftBookApp
+//
+//  Created by Taketoshi Ikegai on 2025/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftBookApp/SwiftBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookAppApp.swift
+//  SwiftBookApp
+//
+//  Created by Taketoshi Ikegai on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
